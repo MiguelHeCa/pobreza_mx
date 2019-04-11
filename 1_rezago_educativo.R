6ddb0d8fb3c8f8f4e64453d305733d8c136b42c3
Miguel Hernandez <jhernandcab@gmail.com>
2019-04-10 21:58:02 -0500

    Loading rds to workspace

--- 1_rezago_educativo.R
+++ 1_rezago_educativo.R
@@ -4,3 +4,11 @@
 # Paquetes ----------------------------------------------------------------
 
 library(tidyverse)
+
+# I. Indicadores de Privación Social
+
+# Indicador de carencia por REZAGO EDUCATIVO ------------------------------
+
+poblacion1 <- readRDS("data/poblacion.rds")
+
+poblacion1 <- rename_all(poblacion1, tolower)
