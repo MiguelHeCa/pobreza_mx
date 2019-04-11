ab2b9e66f9cb3a3d1f3746b0eab71441b3cae976
Miguel Hernandez <jhernandcab@gmail.com>
2019-04-10 22:33:51 -0500

    Filtering population

--- 1_rezago_educativo.R
+++ 1_rezago_educativo.R
@@ -9,6 +9,16 @@ library(tidyverse)
 
 # Indicador de carencia por REZAGO EDUCATIVO ------------------------------
 
-poblacion1 <- readRDS("data/poblacion.rds")
+poblacion_brut <- readRDS("data/poblacion.rds")
 
-poblacion1 <- rename_all(poblacion1, tolower)
+poblacion_brut  <- rename_all(poblacion_brut, tolower)
+
+# Quitar de la población a huéspedes y trabajadores domésticos
+
+poblacion <- poblacion_brut %>% 
+  mutate(parentesco = as.numeric(parentesco)) %>% 
+  filter(!((parentesco >= 400 & parentesco < 500) | 
+             parentesco >= 700 & parentesco < 800))
+
+rm(poblacion_brut)
+gc()
